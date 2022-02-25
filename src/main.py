f22e39bf9603de529a5045741f0c718e2cad2b23
git-personnal <business.vitis@gmail.com>
2022-02-25 06:18:19 +0100

    Using candlestick chart and getting used to plotly / dash with trials

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 class Reader:
     
@@ -8,6 +9,10 @@ class Reader:
 
     def __init__(self, stockName):
         self.dataset = pd.read_csv(Reader.DATA_PATH + '/' + stockName + '.csv')
+        self.daily_variation()
+
+    def daily_variation(self):
+        self.dataset["daily_variation"] = self.dataset["Close"] - self.dataset["Open"]
 
     def get_dataset(self):
         return self.dataset
@@ -17,9 +22,23 @@ if __name__ == '__main__':
 
     aapl = Reader('AAPL').get_dataset()
 
-    fig = px.line(
-        x = aapl["Date"],
-        y = aapl["Volume"]
+    #fig = px.line(
+    #    x = aapl["Date"],
+    #    y = aapl["Close"]
+    #)
+
+    #fig.show()
+
+    fig = go.Figure(go.Candlestick(
+        x = aapl.Date,
+        open = aapl.Open,
+        close = aapl.Close,
+        low = aapl.Low,
+        high = aapl.High
+    ))
+
+    fig.update_layout(
+        title = "AAPL Stocks"
     )
 
     fig.show()
