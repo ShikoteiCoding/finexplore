45f19e7fd24b0ad7c7b173ddbf9460cd40a0188c
git-personnal <business.vitis@gmail.com>
2022-05-11 17:17:11 +0200

    Lot of research: adding url to code base before starting scrapping

--- src/investing/main.py
+++ src/investing/main.py
@@ -1,5 +1,13 @@
 import yfinance as yf
 
+## Intersting URLs to scrap
+
+# Ticker change in names: https://www.nasdaq.com/market-activity/stocks/symbol-change-history
+# PER of companies: https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/pedata.html
+# Financial statements: https://www.sec.gov/dera/data/financial-statement-data-sets.html
+
+
+
 
 if __name__ == '__main__':
     print('hi')
