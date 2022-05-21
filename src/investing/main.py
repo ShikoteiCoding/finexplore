b900dd54636778fbf46107c8d3cf4130f5a70187
git-personnal <business.vitis@gmail.com>
2022-05-21 08:13:25 +0200

    Setup for etfs scrapping

--- src/investing/main.py
+++ src/investing/main.py
@@ -1,13 +1,22 @@
 import yfinance as yf
 
+from utils import load_json
+
 ## Intersting URLs to scrap
 
 # Ticker change in names: https://www.nasdaq.com/market-activity/stocks/symbol-change-history
 # PER of companies: https://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/pedata.html
 # Financial statements: https://www.sec.gov/dera/data/financial-statement-data-sets.html
 
+def main():
+
+    DATA_PATH = 'data'
+    ETF_PATH  = DATA_PATH + '/ETFs.json'
+
+    etf_configs = load_json(ETF_PATH)
+    print(etf_configs)
 
 
 
 if __name__ == '__main__':
-    print('hi')
+    main()
---
+++ src/investing/utils.py
@@ -0,0 +1,6 @@
+import json
+
+def load_json(path: str) -> dict:
+    with open(path) as f:
+        content = json.load(f)
+    return content
