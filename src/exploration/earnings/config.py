be559a1b11e0db4956960ed73a7f43b01d44b588
git-personnal <business.vitis@gmail.com>
2022-08-24 15:13:16 +0200

    Creating a config class to hold for environment variables

---
+++ src/exploration/earnings/config.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import Callable
+
+import os
+
+@dataclass
+class Config:
+    
+    db_username:str = field(default="")
+    db_password:str = field(default="")
+    db_name:str = field(default="")
+    db_host:str = field(default="")
+    db_port:str = field(default="")
+
+def load_config(*opts:Callable) -> Config:
+    c = Config()
+
+    for opt in opts:
+        opt(c)
+
+    return c
+
+def load_db_opts(config: Config) -> None:
+    config.db_username = os.getenv("POSTGRES_USERNAME", "")
+    config.db_password = os.getenv("POSTGRES_PASSWORD", "")
+    config.db_name = os.getenv("POSTGRES_DB_NAME", "")
+    config.db_host = os.getenv("POSTGRES_DB_HOST", "")
+    config.db_port = os.getenv("POSTGRES_DB_PORT", "")
--- src/exploration/earnings/main.py
+++ src/exploration/earnings/main.py
@@ -6,10 +6,17 @@ import matplotlib.pyplot as plt
 import utils
 import psycopg2
 
+from config import Config, load_config as load, load_db_opts
+
 if __name__ == "__main__":
 
-    res = utils.load_sp_500_constituents(reload=False)
-    top_5_symbols = [row.symbol for _, row in res[:5].iterrows()]
-    earnings = utils.load_ticker_earnings_history(top_5_symbols, reload=False)
-    
-    print(utils.enrich_tickers_earnings_history(earnings))
+    #res = utils.load_sp_500_constituents(reload=False)
+    #top_5_symbols = [row.symbol for _, row in res[:5].iterrows()]
+    #earnings = utils.load_ticker_earnings_history(top_5_symbols, reload=False)
+    #
+    #print(utils.enrich_tickers_earnings_history(earnings))
+
+    config = load(load_db_opts)
+
+
+    print(config)
