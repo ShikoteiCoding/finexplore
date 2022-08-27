9be4aa5f7fd72394a519b30b4519503cdc5424fd
git-personnal <business.vitis@gmail.com>
2022-08-27 17:11:16 +0200

    Calling Polygon API and saving (to make a parsing function before pushing to SQL)

--- src/exploration/earnings/main.py
+++ src/exploration/earnings/main.py
@@ -1,14 +1,12 @@
 import utils
-import pandas as pd
-import datetime
-import yfinance as yf
-import matplotlib.pyplot as plt
 import utils
 import psycopg2
 import dotenv
+import requests
 from psycopg2 import sql
+import json
 
-from config import Config, load_config as load, load_db_opts, load_polygon_opts
+from config import load_config as load, load_db_opts, load_polygon_opts
 
 if __name__ == "__main__":
     # Load local dotenv (because not dockerize for now - can be added to image or passed at docker run)
@@ -21,5 +19,15 @@ if __name__ == "__main__":
     tickers = ["MMM"]
     #print(utils._scrap_opening_minutes_earning_date(tickers[0], datetime.datetime(2022, 7, 26), datetime.datetime(2022, 7, 26)))
     print(config)
+
+    url = f"https://api.polygon.io/v2/aggs/ticker/MMM/range/1/minute/2022-07-26/2022-07-26?adjusted=false&sort=asc&limit=120&apiKey={config.polygon_access_key}"
+
+    
+    file = "data/test.txt"
+    #with open(file, "w") as f:
+    #    res = requests.get("https://api.polygon.io/v2/aggs/ticker/MMM/range/1/minute/2022-07-26/2022-07-26?adjusted=false&sort=asc&limit=120&apiKey=KFogwckHT4l3GcS4f7IZEjwbO6DRg_3a")
+    #    json_object = json.dumps(res.json(), indent=4)
+    #    f.write(json_object)
+    print(url)
 
     #connection.close()
