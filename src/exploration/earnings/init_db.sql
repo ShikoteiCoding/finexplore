884d0a9abefc4fd6b34f8d0dd42a7ec9cb0291be
git-personnal <business.vitis@gmail.com>
2022-08-29 05:20:38 +0200

    Adding a table for minute prices

--- src/exploration/earnings/init_db.sql
+++ src/exploration/earnings/init_db.sql
@@ -44,3 +44,17 @@ CREATE TABLE tickers_daily_share_prices (
 );
 
 CREATE UNIQUE INDEX unique_tickers_daily_share_prices ON tickers_daily_share_prices (date, symbol);
+
+CREATE TABLE tickers_minute_share_prices (
+    date timestamp without time zone,
+    symbol text,
+    open double precision,
+    high double precision,
+    low double precision,
+    close double precision,
+    volume double precision,
+    dividends double precision,
+    stock_splits double precision
+);
+
+CREATE UNIQUE INDEX unique_tickers_minute_share_prices ON tickers_minute_share_prices (date, symbol);
