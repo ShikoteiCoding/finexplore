1998ce61da1a508a33f88a5ef3c9b3cb4ab9ac89
git-personnal <business.vitis@gmail.com>
2022-08-29 04:55:09 +0200

    First simple version of polygon api call

--- src/exploration/earnings/utils.py
+++ src/exploration/earnings/utils.py
@@ -280,15 +280,20 @@ def _scrap_daily_prices_for_earnings(symbol:str, start_day:datetime.datetime, en
 
     return df
 
-def _scrap_opening_minutes_earning_date(symbol: str, start:datetime.datetime, end:datetime.datetime, *, metadata:dict = METADATA["minute_prices"]) -> pd.DataFrame:
+def _scrap_opening_minutes_earning_date(symbol: str, start:datetime.datetime, end:datetime.datetime, *, config:Config, metadata:dict = METADATA["minute_prices"]) -> dict:
     """ Scrap the first minutes following earningq. Using yfinance library. """
-    ticker = Ticker(symbol)
 
-    df:pd.DataFrame = ticker.history(start=start, end=end, interval="1m", auto_adjust=False, back_adjust=False)
+    url = f"https://api.polygon.io/v2/aggs/ticker/MMM/range/1/minute/2022-07-26/2022-07-26?adjusted=false&sort=asc&limit=120&apiKey={config.polygon_access_key}"
 
-    return df
-    
-    
+    data = {}
+
+    try:
+        res = requests.get(url)
+        data = res.json()
+    except Exception as e:
+        print(f"[ERROR]: Failed to scrap polyugon. \n{e}")
+
+    return data
 
 def _scrap_monthly_prices(symbol:str, start_date:datetime.datetime, end_date:datetime.datetime, metadata:dict = METADATA["monthly_prices"]) -> pd.DataFrame:
     """ Scrap the monthly prices. Using yfinance library. """
