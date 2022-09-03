0dca892f8ff3956fab78322312b2a577c7c57981
git-personnal <business.vitis@gmail.com>
2022-09-03 06:08:07 +0200

    Beautify printing function for easier log reading

--- src/exploration/earnings/utils.py
+++ src/exploration/earnings/utils.py
@@ -41,7 +41,22 @@ OPENING_HOURS = {
         "end": "13:00"
     }
 }
+
 #--------------------------
+
+def printable_banner(text:str, length:int=50) -> str:
+    """ Print a beautified banner. """
+    assert type(length) == int, "Please length should be an int."
+
+    if len(text) > length:
+        length = len(text) + 10
+    
+    odd = (length - len(text)) % 2
+
+    border = "#" * (((length - len(text)) // 2) + odd - 1)
+
+    output = "\n" + "#" * (length + odd) + "\n" + border + " " + text + " " + border + "\n" + "#" * (length + odd)
+    return output
 
 def str_to_hour(hour:str, format:str = "%H:%M") -> datetime.time:
     return datetime.datetime.strptime(hour, format).time()
