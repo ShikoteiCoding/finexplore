fa61fb8e6763bbb8418188b55c8e0709e7b6cf06
git-personnal <business.vitis@gmail.com>
2022-11-16 16:36:58 +0800

    Connecting SDK to API

--- buxzero/_utils.py
+++ buxzero/_utils.py
@@ -1,9 +1,26 @@
-#import bux
+import os
 
-#from ._base import Command, register
 from _user import GuestAPI
+from dotenv import load_dotenv
 
-def get_token() -> str:
+def get_token(env_path="token.env") -> str:
+    """
+    Get the token either in file or environment variables
+    """
+    try:
+        load_dotenv("token.env")
+    except Exception as e:
+        print(f"No environment file found: {e}")
+    token = os.getenv("BUX_TOKEN")
+    if not token:
+        return get_token_command()
+
+    return token
+
+def get_token_command() -> str:
+    """
+    Logic to require token necessities to user.
+    """
     email = input('1. Enter email: ')
     api = GuestAPI()
     api.request_link(email).requests()
--- buxzero/main.py
+++ buxzero/main.py
@@ -3,8 +3,13 @@ import os
 from _config import Config
 from _user import UserAPI
 
+from _utils import get_token
+
 if __name__ == "__main__":
 
     c = Config()
 
-    user = UserAPI(token="")
+    token = get_token()
+    user = UserAPI(token=token)
+
+    print(user.me().requests())
