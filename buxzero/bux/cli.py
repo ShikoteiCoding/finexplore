1ddb4d8c152de119cb8f17863360131a594c33e9
git-personnal <business.vitis@gmail.com>
2022-11-23 10:45:39 +0800

    Removing prints

--- buxzero/bux/cli.py
+++ buxzero/bux/cli.py
@@ -6,21 +6,18 @@ from .commands import commands
 
 
 def main(argv: list[str], stream: TextIO) -> int:
-    parser = ArgumentParser("python3 -m commands")
+    parser = ArgumentParser("python3 -m bux")
     subparsers = parser.add_subparsers()
     parser.set_defaults(cmd=None)
 
     for name, cmd_class in commands.items():
-        print(name, cmd_class)
         subparser = subparsers.add_parser(name=name)
         subparser.set_defaults(cmd=cmd_class)
         cmd_class.init_parser(subparser)
     args = parser.parse_args(argv)
 
-    print(args)
-
     cmd_class = args.cmd
-    if cmd_class is None:  # pragma: no cover
+    if cmd_class is None:
         parser.print_help()
         return 1
     cmd = cmd_class(args=args, stream=stream)
