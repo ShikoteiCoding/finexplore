fa9f0feeaca81a70dba45a78442b3a776b5ddf72
git-personnal <56791111+ShikoteiCoding@users.noreply.github.com>
2022-11-25 14:14:53 +0800

    Improving test of Me response

--- buxzero/tests/helpers.py
+++ buxzero/tests/helpers.py
@@ -1,14 +1,40 @@
 import bux
 from datetime import datetime
-
+from jsonpath_ng import jsonpath, parse
 from decimal import Decimal
+from typing import Any
 
-##############################
-###### TODO: Should be able to check for nested fields and nest values nativelue. Use jsonpath-ng ?
-##############################
+import pandas as pd
 
+def flatten_dict(obj: dict) -> dict:
+    output = {}
 
-def assert_fields_have_getters(response: bux.Response, exclude=(), unwrap=()):
+    def flatten(item: dict | list | Any, name: str=''):
+        if type(item) is dict:
+            for a in item:
+                flatten(item[a], name + a + '.')
+        elif type(item) is list:
+            i = 0
+            for a in item:
+                flatten(a, name + str(i) + '.')
+                i += 1
+        else:
+            output[name[:-1]] = item
+
+    flatten(obj)
+    return output 
+
+def assert_fields_exist(response: bux.Response, fields:set[str]=set()):
+    """ Assert json paths exist in object. """
+    uncalled = []
+    for field in fields:
+        json_path = parse(field)
+        value = json_path.find(response)
+        if not value:
+            uncalled.append(field)
+    assert not uncalled, "Missing fields: " + ' '.join(uncalled)
+
+def assert_fields_have_getters(response: bux.Response, exclude:set[str]=set()):
     """ Assert all response fields have getters to access. """
 
     # Call each declared properties
@@ -19,26 +45,24 @@ def assert_fields_have_getters(response: bux.Response, exclude=(), unwrap=()):
             continue
         if prop.startswith("_"):
             continue
-        values.append(getattr(response, prop))
+        value = getattr(response, prop)
+        if not value:
+            continue
+        values.append(value)
 
-    # Compare actual values to called properties
+    # Compare response actual values to called properties values
     uncalled = []
-    for key, value in response.items():
+    actual_values = []
+    for key, value in flatten_dict(dict(response)).items():
         if key in exclude:
             continue
-        if key in unwrap:
-            continue
+        actual_values.append(value)
         if value not in values:
             uncalled.append(key)
+        
+    # Check called property values not excluded are same as property values
+    unfound = set(sorted(values)) - set(sorted(actual_values))
 
     # Check for nested fields
-    for subkey in unwrap:
-        for key, value in response[subkey].items():
-            if key in exclude:
-                continue
-            if key in unwrap:
-                continue
-            if value not in values:
-                uncalled.append(key)
-
-    assert not uncalled, "Missing fields: " + ' '.join(uncalled)
+    assert not uncalled, "Missing fields: " + " ".join(uncalled)
+    assert not unfound, "Property values not found: " + " ".join(unfound)
--- buxzero/tests/test_user.py
+++ buxzero/tests/test_user.py
@@ -1,30 +1,36 @@
 import pytest
 
 import bux
-from .helpers import assert_fields_have_getters
+from .helpers import assert_fields_exist, assert_fields_have_getters
 from .fixtures import api, config
 
 
 def test_me(api: bux.UserAPI) -> None:
     response = api.me.requests()
     fields = {
-        'accountStatus',
-        'usMarketDataSubscriptionActivated',
-        'pinStatus',
-        'communicationConfiguration',
-        'profile',
-        'etfAgreementAccepted',
-        'accountType',
-        'reassessmentInfo',
+        "accountStatus",
+        "accountType",
+        "communicationConfiguration.monthlyTransactionsReportingEnabled",
+        "etfAgreementAccepted",
+        "pinStatus",
+        "profile.nickname",
+        "profile.userId",
+        "reassessmentInfo.required",
+        "usMarketDataSubscriptionActivated",
     }
-    assert set(response) == fields
+    assert_fields_exist(response, fields)
 
-    assert response.account_status == 'READY'
-    assert response.account_type == 'USER'
-    assert response.pin_status == 'ENABLED'
+    assert response.account_status == "READY", "The provided account is not ready to use."
+    assert response.account_type == "USER"
+    assert response.pin_status == "ENABLED"
 
     assert_fields_have_getters(
         response, 
-        exclude={"usMarketDataSubscriptionActivated", "etfAgreementAccepted", "communicationConfiguration", "reassessmentInfo", "nickname"},
-        unwrap={"profile"}
+        exclude={
+            "usMarketDataSubscriptionActivated", 
+            "etfAgreementAccepted", 
+            "communicationConfiguration.monthlyTransactionsReportingEnabled", 
+            "reassessmentInfo.required", 
+            "profile.nickname"
+        }
     )
