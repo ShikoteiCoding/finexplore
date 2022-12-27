8413dd0d78ad03b0ac7eb2cd665d73370d9fdf7a
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2022-12-27 14:26:43 +0800

    Adding output cmd parse options

--- buxzero/bux/commands/_portfolio.py
+++ buxzero/bux/commands/_portfolio.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 
 import bux
+import json
 
 from ._command import Command, register
 
@@ -11,14 +12,27 @@ class Portfolio(Command):
 
     @staticmethod
     def init_parser(parser: ArgumentParser) -> None:
-        parser.add_argument("--config", required=False)
+        parser.add_argument("-c", "--config", required=False)
+        parser.add_argument(
+            "-o",
+            "--output",
+            required=False,
+            default="console",
+            choices=["json", "console"],
+        )
 
     def run(self) -> int:
         if self.args.config:
             self.load_config_env(file_path=str(self.args.config))
         else:
             self.load_config_env()
+
         api = bux.UserAPI(config=self.config)
         portfolio = api.portfolio.requests()
-        print(portfolio)
+
+        if self.args.output == "json":
+            print(json.dumps(dict(portfolio)))
+        elif self.args.output == "console":
+            print(portfolio)
+
         return 0
